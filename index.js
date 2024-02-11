8fcbaa7dc4a0c753ba72fcd0126300b413d8f507
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-02-12 01:46:48 +0300

    array-object loop

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function showYoungestStudent(students) { }
+function showYoungestStudent(students) { 
+var youngest = students[0];
+    for(var i = 1; i < students.length; i++){
+    if( students[i].age < youngest.age){
+        youngest = students[i]
+    }
+}
+console.log("This student youngest of other student");
+console.log(youngest);
+}
 
-module.exports = showYoungestStudent;
+var students = [{name:"Noor", age:23},
+    {name:"Reem", age:22},
+    {name:"Huda", age:17},
+    {name:"Alaa", age:22},
+    {name:"Ali", age:20},
+    {name:"Omnia", age:21},
+    {name:"Nabel", age:33},
+    {name:"Arkan", age:19}]
+    
+showYoungestStudent(students);
